2ceb17f9d84005d9f5f07edf9eeedf3266d0c60c
shivrajstart <shivrajstart@live.com>
2021-02-08 00:42:20 +0530

    Added AuthorController and few Api endpoints

---
+++ BookStore-API/Controllers/AuthorsController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using BookStore_API.Contracts;
+using BookStore_API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// Endpoint used to interact with the Authors in the bokk store's database
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
+    [Route("api/[controller]")]
+    [ApiController]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IAuthorRepository _authorRepository;
+        private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
+        public AuthorsController(IAuthorRepository authorRepository, ILoggerService loggerService, IMapper mapper)
+        {
+            _authorRepository = authorRepository;
+            _logger = loggerService;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Gets All Authors.
+        /// </summary>
+        /// <returns>List of Authors</returns>
+        [HttpGet]
+        public async Task<IActionResult> GetAuthors()
+        {
+            try
+            {
+                _logger.LogInfo("Attemped GetAll Authors");
+                var authors = await _authorRepository.FindAll();
+                var response = _mapper.Map<IList<AuthorDTO>>(authors);
+                _logger.LogInfo("Successfully got all Authors");
+                return Ok(response);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{ex.Message} - {ex.InnerException}");
+                return StatusCode(500, "Something went wrong. Please contact the Administrator");
+            }
+        }
+    }
+}
---
+++ BookStore-API/Services/AuthorRepository.cs
@@ -0,0 +1,55 @@
+﻿using BookStore_API.Contracts;
+using BookStore_API.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Services
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public AuthorRepository(ApplicationDbContext applicationDbContext)
+        {
+            dbContext = applicationDbContext;
+        }
+        public async Task<bool> Create(Author entity)
+        {
+            await dbContext.Authors.AddAsync(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Delete(Author entity)
+        {
+            dbContext.Authors.Remove(entity);
+            return await Save();
+        }
+
+        public async Task<IList<Author>> FindAll()
+        {
+            var authors = await dbContext.Authors.ToListAsync();
+            return authors;
+        }
+
+        public async Task<Author> FindById(int id)
+        {
+            var author = await dbContext.Authors.FindAsync(id);
+            return author;
+        }
+
+        public async Task<bool> Save()
+        {
+            var changes = await dbContext.SaveChangesAsync();
+            return changes > 0;
+        }
+
+        public async Task<bool> Update(Author entity)
+        {
+            dbContext.Authors.Update(entity);
+            return await Save();
+        }
+    }
+}
